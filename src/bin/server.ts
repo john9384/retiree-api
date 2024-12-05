214523335a93eec284e8caf9e079462f013af532
John Damilola Ogungbure <jogungbure@gmail.com>
2024-12-05 12:01:31 +0100

    cors

--- src/bin/server.ts
+++ src/bin/server.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../app/app.module';
 import { GlobalExceptionFilter } from 'library/middleware/global-exception-filter';
+import { config } from 'config/index';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: [config.frontendBase],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const config = {
   appName: process.env.APP_NAME,
   port: process.env.PORT,
-
+  frontendBase: process.env.FRONTEND_BASE,
   jwtSecret: process.env.JWT_SECRET,
   jwtExpiresIn: process.env.JWT_EXPIRES_IN,
 
